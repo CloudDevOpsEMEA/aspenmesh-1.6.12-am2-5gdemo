533d3352a02d397b78bca963d69818b0dbf2bb56
Bart Van Bos <bartvanbos@gmail.com>
2020-11-04 17:31:00 +0100

    enable jaeger

--- udf/aspenmesh/install.sh
+++ udf/aspenmesh/install.sh
@@ -4,3 +4,4 @@ helm install istio-base ../../manifests/charts/base --namespace istio-system
 helm install istiod ../../manifests/charts/istio-control/istio-discovery --namespace istio-system --values udf-values.yaml
 helm install istio-ingress ../../manifests/charts/gateways/istio-ingress --namespace istio-system --values udf-values.yaml
 helm install istio-egress ../../manifests/charts/gateways/istio-egress --namespace istio-system --values udf-values.yaml
+helm install istio-telemetry ../../manifests/charts/istio-telemetry/grafana --namespace istio-system --values udf-values.yaml
