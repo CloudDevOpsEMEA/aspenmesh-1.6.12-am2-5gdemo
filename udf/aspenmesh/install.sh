51afbde2f6af0518f25fb2a87d3fbbecdcfb07e7
Bart Van Bos <bartvanbos@gmail.com>
2020-11-04 19:05:34 +0100

    expose prometheus and grafana

--- udf/aspenmesh/install.sh
+++ udf/aspenmesh/install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+kubectl create namespace istio-system
+
 helm install istio-base manifests/charts/base --namespace istio-system
 helm install istiod manifests/charts/istio-control/istio-discovery --namespace istio-system --values udf/aspenmesh/udf-values.yaml
 helm install istio-ingress manifests/charts/gateways/istio-ingress --namespace istio-system --values udf/aspenmesh/udf-values.yaml
 helm install istio-egress manifests/charts/gateways/istio-egress --namespace istio-system --values udf/aspenmesh/udf-values.yaml
-helm install istio-telemetry manifests/charts/istio-telemetry/grafana --namespace istio-system --values udf/aspenmesh/udf-values.yaml
+# helm install istio-telemetry manifests/charts/istio-telemetry/grafana --namespace istio-system --values udf/aspenmesh/udf-values.yaml
--- udf/aspenmesh/uninstall.sh
+++ udf/aspenmesh/uninstall.sh
@@ -4,4 +4,4 @@ helm uninstall istio-base --namespace istio-system
 helm uninstall istiod --namespace istio-system
 helm uninstall istio-ingress --namespace istio-system
 helm uninstall istio-egress --namespace istio-system
-helm uninstall istio-telemetry --namespace istio-system
+# helm uninstall istio-telemetry --namespace istio-system
