427ac03e3f5379657ff4b70302b7732b505b1139
Bart Van Bos <bartvanbos@gmail.com>
2020-11-04 17:35:50 +0100

    enable jaeger

--- udf/aspenmesh/install.sh
+++ udf/aspenmesh/install.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-helm install istio-base ../../manifests/charts/base --namespace istio-system
-helm install istiod ../../manifests/charts/istio-control/istio-discovery --namespace istio-system --values udf-values.yaml
-helm install istio-ingress ../../manifests/charts/gateways/istio-ingress --namespace istio-system --values udf-values.yaml
-helm install istio-egress ../../manifests/charts/gateways/istio-egress --namespace istio-system --values udf-values.yaml
-helm install istio-telemetry ../../manifests/charts/istio-telemetry/grafana --namespace istio-system --values udf-values.yaml
+helm install istio-base manifests/charts/base --namespace istio-system
+helm install istiod manifests/charts/istio-control/istio-discovery --namespace istio-system --values udf/aspenmesh/udf-values.yaml
+helm install istio-ingress manifests/charts/gateways/istio-ingress --namespace istio-system --values udf/aspenmesh/udf-values.yaml
+helm install istio-egress manifests/charts/gateways/istio-egress --namespace istio-system --values udf/aspenmesh/udf-values.yaml
+helm install istio-telemetry manifests/charts/istio-telemetry/grafana --namespace istio-system --values udf/aspenmesh/udf-values.yaml
+
+helm upgrade istio-base manifests/charts/base --namespace istio-system
+helm upgrade istiod manifests/charts/istio-control/istio-discovery --namespace istio-system --values udf/aspenmesh/udf-values.yaml
+helm upgrade istio-ingress manifests/charts/gateways/istio-ingress --namespace istio-system --values udf/aspenmesh/udf-values.yaml
+helm upgrade istio-egress manifests/charts/gateways/istio-egress --namespace istio-system --values udf/aspenmesh/udf-values.yaml
+helm upgrade istio-telemetry manifests/charts/istio-telemetry/grafana --namespace istio-system --values udf/aspenmesh/udf-values.yaml
