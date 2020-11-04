4aa295181bf3f7c01d7ce696f8a5359ce555dc67
Bart Van Bos <bartvanbos@gmail.com>
2020-11-04 17:45:04 +0100

    enable jaeger

---
+++ udf/aspenmesh/uninstall.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+helm uninstall istio-base --namespace istio-system
+helm uninstall istiod --namespace istio-system
+helm uninstall istio-ingress --namespace istio-system
+helm uninstall istio-egress --namespace istio-system
+helm uninstall istio-telemetry --namespace istio-system
